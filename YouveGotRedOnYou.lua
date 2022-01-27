fe4e0a541ee643895d1773c17be94516c5d30686
McPewPew <98543024+McPewPew@users.noreply.github.com>
2022-01-27 20:34:57 +0000

    Tested  passed

--- YouveGotRedOnYou.lua
+++ YouveGotRedOnYou.lua
@@ -9,7 +9,7 @@ WMPVP:SetScript("OnEvent", function()
                UIErrorsFrame:AddMessage(wrmpvpmsg, 1.0, 0.0, 0.0, 53, 5)
 			   PlaySoundFile("Sound/Character/Gnome/GnomeVocalMale/GnomeMaleNo03.wav")
             end
-		 elseif  UnitIsFriend("player", "target") then
+		 elseif  not UnitIsFriend("player", "target") then
 		    trgtisscry = UnitClassification("target")
 			if trgtisscry == "worldboss" or trgtisscry == "rareelite" or trgtisscry == "elite" then
 				wrmpvpmsg = (UnitName("target") .. " is " .. trgtisscry .."!")
