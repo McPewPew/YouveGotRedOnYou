075ea17dad12b1f745dcbf62ae3370bcd23bb2ad
McPewPew <98543024+McPewPew@users.noreply.github.com>
2022-01-27 19:05:23 +0000

    Initial commit

---
+++ YouveGotRedOnYou.lua
@@ -0,0 +1,25 @@
+WMPVP = CreateFrame("Frame", nil, nil)  
+WMPVP:RegisterEvent("PLAYER_TARGET_CHANGED")
+
+WMPVP:SetScript("OnEvent", function()
+      if not isInstance() then
+         if UnitIsPlayer("target") then
+            if UnitIsPVP("target") then
+               wrmpvpmsg = (UnitName("target") .. "is PVP flagged!")
+               UIErrorsFrame:AddMessage(wrmpvpmsg, 1.0, 0.0, 0.0, 53, 5)
+			   PlaySoundFile("Sound/Character/Gnome/GnomeVocalMale/GnomeMaleNo03.wav")
+            end
+		 elseif  UnitIsFriend("player", "target") then
+		    trgtisscry = UnitClassification("target")
+			if trgtisscry == "worldboss" or trgtisscry == "rareelite" or trgtisscry == "elite" then
+				wrmpvpmsg = (UnitName("target") .. " is " .. trgtisscry .."!")
+				UIErrorsFrame:AddMessage(wrmpvpmsg, 1.0, 0.0, 0.0, 53, 5)
+				PlaySoundFile("Sound/Character/Gnome/GnomeVocalMale/GnomeMaleFlee01.wav")	
+			elseif trgtisscry == "rare" then
+				wrmpvpmsg = (UnitName("target") .. " is " .. trgtisscry .."!")
+				UIErrorsFrame:AddMessage(wrmpvpmsg, 1.0, 0.0, 1.0, 53, 5)
+				PlaySoundFile("Sound/Character/Gnome/GnomeVocalMale/GnomeMaleCheer02.wav")
+			end	
+         end
+      end  
+end)
